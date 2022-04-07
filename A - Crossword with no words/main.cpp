21b49af114519150e105eeeb18061e38485a1509
Filip3ra <filipimacielrj@gmail.com>
2022-04-07 09:38:23 -0300

    primeira abordagem

--- A - Crossword with no words/main.cpp
+++ A - Crossword with no words/main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
 
-    
+    int N=1, M=1, K=1;
+    int mat[2001][2001]{0};
+
+    while(N!=0){
+
+        cin >> N >> M >> K;
+
+        cout << N << M << K << endl;
+
+    }
 
     return 0;
 }
